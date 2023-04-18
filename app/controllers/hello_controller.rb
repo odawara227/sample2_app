45e4d0697494629d2fc6da9b89b629749cdddfa4
odawara227 <maijimanew227@gmail.com>
2023-04-18 12:36:03 +0900

    message

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -32,21 +32,50 @@
     # end
     
     
-#    #def index
-#         @title = "View Sample"
-#         @msg = "コントローラーに用意した値です。"
-#     end
+    #    #def index
+    #         @title = "View Sample"
+    #         @msg = "コントローラーに用意した値です。"
+    #     end
 
+    # #    def index
+    #         if params['msg'] != nil then
+    #             @title = params['msg']
+    #         else
+    #             @title = 'index'
+    #         end
+    #         @msg = 'this is redirect sample...'
+    #     end
+
+    #     def other
+    #         redirect_to action: :index, params: {'msg': 'from other page'}
+    #     end
+
+    # def index
+    #     msg = '
+    #     <html>
+    #     <body>
+    #         <h1>Sample Page</h1>
+    #         <p>this is Rails sample page!</p>
+    #     </body>
+    #     </html>
+    #     '
+    #     render html: msg.html_safe
+    # end
     def index
         if params['msg'] != nil then
-            @title = params['msg']
+            msg = 'Hello, ' + params['msg'] + '!'
         else
-            @title = 'index'
+            msg = 'this is sample page.'
         end
-        @msg = 'this is redirect sample...'
-    end
 
-    def other
-        redirect_to action: :index, params: {'msg': 'from other page'}
+        html = '
+        <html>
+        <body>
+            <h1>Sample Page</h1>
+            <p>' + msg + '</p>
+        </body>
+        </html>
+        '
+        render html: html.html_safe
     end
 end
